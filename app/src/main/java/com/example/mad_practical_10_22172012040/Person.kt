ed4bbb6bf3f87366054ec43ed2830e1cd3ac37ad
student <student@gnu.ac.in>
2023-10-25 08:52:04 +0530

    io

--- app/src/main/java/com/example/mad_practical_10_22172012040/Person.kt
+++ app/src/main/java/com/example/mad_practical_10_22172012040/Person.kt
@@ -1,5 +1,6 @@
 package com.example.mad_practical_10_22172012040
 
+import org.json.JSONObject
 import java.io.Serializable
 
 class Person(var id:String,
@@ -9,5 +10,17 @@ var PhoneNo : String,
 var Address : String,
 var latitude : Double,
 var longutude : Double):Serializable{
+
+    constructor(jsonObject: JSONObject):this("","","","","",0.0,0.0) {
+        id = jsonObject.getString("id")
+        emailId = jsonObject.getString("email")
+        PhoneNo = jsonObject.getString("phone")
+        val profileJson = jsonObject.getJSONObject("profile")
+        name = profileJson.getString("name")
+        Address = profileJson.getString("address")
+        val locationJson = profileJson.getJSONObject("location")
+        latitude = locationJson.getDouble("lat")
+        longutude = locationJson.getDouble("long")
+    }
 
 }
